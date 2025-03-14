e83f924e1d461e29dbd0c4b637f06a7fcb871472
24LP5569 24LP5569 <24LP5569@G304-CC0226.local>
2025-03-14 12:32:50 +0800

    cat foods

--- src/app/_components/CategoryFoods.tsx
+++ src/app/_components/CategoryFoods.tsx
@@ -1,8 +1,16 @@
 "use client";
-import { Divide } from "lucide-react";
 import { AddNewDish } from "./AddNewDish";
 import { FoodCategoryType, FoodType } from "../_util/type";
 import { useEffect, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Pencil } from "lucide-react";
 
 const CategoryFoods = ({ category }: { category: FoodCategoryType }) => {
   const [foods, setFoods] = useState<FoodType[]>([]);
@@ -25,21 +33,38 @@ const CategoryFoods = ({ category }: { category: FoodCategoryType }) => {
         <AddNewDish name={category.categoryName} />
       </div>
       <div className="flex felx-col gap-4 pt-10">
-        {foods?.map((food: FoodType, index: number) => {
-          return (
-            <div
-              key={index}
-              className="w-[250px] h-[250px] rounded-[20px] border-[gray] border flex flex-col justify-center items-center gap-5"
-            >
-              <div className="w-[200px] h-[200px] bg-[#EF4444] pt-[20px] text-white flex justify-center items-center"></div>
-              <div className="flex gap-3">
-                <p>{food.foodName}</p>
-                <p>{food.price}</p>
+        <Dialog>
+          Open
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you absolutely sure?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+          {foods?.map((food: FoodType, index: number) => {
+            return (
+              <div
+                key={index}
+                className="w-[250px] h-[250px] rounded-[20px] border-[gray] border flex flex-col justify-center items-center gap-5"
+              >
+                <div className="w-[200px] h-[200px] bg-[#EF4444] pt-[20px] text-white flex justify-center items-center"></div>
+                <div className="flex gap-3 flex-col">
+                  <DialogTrigger>
+                    <div className="w-[35px] h-[35px] rounded-full bg-[#EF4444] text-[white] flex justify-center items-center">
+                      <Pencil />
+                    </div>
+                  </DialogTrigger>
+                  <p>{food.foodName}</p>
+                  <p>{food.price}</p>
+                </div>
+                <p>{food.ingredietnts}</p>
               </div>
-              <p>{food.ingredietnts}</p>
-            </div>
-          );
-        })}
+            );
+          })}
+        </Dialog>
       </div>
     </div>
   );
